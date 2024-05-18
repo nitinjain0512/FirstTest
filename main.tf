7392319692e340a97833dc3aae2bfcb9e84319a9
nitinjain0512 <nitin.jain0512@gmail.com>
2024-05-18 14:18:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_service_account" "service_account" {
-  account_id   = "service-account-id"
-  display_name = "Service Account"
+  account_id   = "svc-uat"
+  display_name = "svc-uat-sa"
 }
 
