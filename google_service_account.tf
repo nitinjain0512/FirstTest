bf412be3cec39bda4af78fd08cd36fc94b36972a
nitinjain0512 <nitin.jain0512@gmail.com>
2024-05-18 14:10:38 +0530

    Update google_service_account.tf

--- google_service_account.tf
+++ google_service_account.tf
@@ -7,3 +7,10 @@ import {
   id = "projects/{{logical-zephyr-423609-u7}}/serviceAccounts/{{nitin.n.jain@gmail.com}}"
   to = google_service_account.default
 }
+
+
+resource "google_project_iam_member" "firestore_owner_binding" {
+  project = logical-zephyr-423609-u7
+  role    = "roles/datastore.owner"
+  member  = "serviceAccount:${nitin.n.jain@gmail.coml}"
+}
