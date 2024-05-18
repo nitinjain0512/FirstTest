e236540477254962427be29fd80c2522febfdd3a
nitinjain0512 <nitin.jain0512@gmail.com>
2024-05-18 14:07:42 +0530

    Update google_service_account.tf

--- google_service_account.tf
+++ google_service_account.tf
@@ -2,3 +2,8 @@ resource "google_service_account" "service_account" {
   account_id   = "service-account-id"
   display_name = "Service Account"
 }
+
+import {
+  id = "projects/{{logical-zephyr-423609-u7}}/serviceAccounts/{{nitin.n.jain@gmail.com}}"
+  to = google_service_account.default
+}
