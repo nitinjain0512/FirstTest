7e76882d89de1e87ad8ca982f2fcd16371e0fe63
nitinjain0512 <nitin.jain0512@gmail.com>
2024-05-19 12:37:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,3 @@ resource "google_service_account" "service_account" {
   display_name = "svc-uat-sa"
 }
 
-resource "google_project_iam_binding" "firestore_owner_binding" {
-  role               = "roles/serviceusage.serviceUsageConsumer"
-  project = var.gcp_project
-  depends_on = [google_service_account.service_account]
-}
