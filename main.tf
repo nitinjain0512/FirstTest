fcfaec642c83747b9d4e298ba1001dd92f2c04a2
nitinjain0512 <nitin.jain0512@gmail.com>
2024-05-19 13:32:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ resource "google_service_account" "service_account" {
 resource "google_project_iam_member" "firestore_owner_binding" {
   project = var.gcp_project
   role    = "roles/datastore.owner"
-  member  = "serviceAccount:${google_service_account.account_id}"
+  member  = "serviceAccount:${google_service_account.service_account}"
 }
